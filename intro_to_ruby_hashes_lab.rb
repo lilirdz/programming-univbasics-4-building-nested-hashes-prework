8786d55150febd1e9c2ff0b44021cd5236950acd
Lili Rodriguez <lilimrod@gmail.com>
2020-03-12 05:15:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,6 +12,8 @@ def monopoly_with_second_tier
   # complexity.
   new_hash = {:railroads => {}}
   new_hash[:railroads][:pieces] = 4
+  new_hash[:railroads][:rent_in_dollars] = {}
+  new_hash[:railroads][:names] = {}
 end
 
 def monopoly_with_third_tier
